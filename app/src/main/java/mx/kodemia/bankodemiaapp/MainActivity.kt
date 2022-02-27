f5bcea07aa7ba0793cc8fe72b8a7b50bdad893d8
edwin mendez <edwin.mendez.an@gmail.com>
2022-02-27 12:19:05 -0600

    Inicio pantalla Iniciar Sesion

--- app/src/main/java/mx/kodemia/bankodemiaapp/MainActivity.kt
+++ app/src/main/java/mx/kodemia/bankodemiaapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package mx.kodemia.bankodemiaapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.kodemia.bankodemiaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    binding.btnBienvenidaInisiarSesion.setOnClickListener {
+
     }
+    }
+
+
+
+
+
+
+//    fun lanzarActivity(){
+//        val intent = Intent(this, )
+//
+//    }
 }
