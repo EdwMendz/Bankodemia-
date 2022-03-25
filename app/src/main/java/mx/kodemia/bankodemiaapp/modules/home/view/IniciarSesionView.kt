08318033e97d0fc45d5526adfa5602f4476b2d44
edwin mendez <edwin.mendez.an@gmail.com>
2022-03-25 13:59:18 -0600

    Se realiza Loginview y LoginViewModel

--- app/src/main/java/mx/kodemia/bankodemiaapp/modules/home/view/IniciarSesionView.kt
+++ app/src/main/java/mx/kodemia/bankodemiaapp/modules/home/view/IniciarSesionView.kt
@@ -4,36 +4,82 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import kotlinx.android.synthetic.main.activity_iniciar_sesion.*
 import mx.kodemia.bankodemiaapp.MainActivity
+import mx.kodemia.bankodemiaapp.R
+import mx.kodemia.bankodemiaapp.core.checkForInternet
+import mx.kodemia.bankodemiaapp.data.model.request.LogInRequest
 import mx.kodemia.bankodemiaapp.databinding.ActivityIniciarSesionBinding
 import mx.kodemia.bankodemiaapp.modules.home.viewmodel.IniciarSesionViewModel
 
 class IniciarSesionView : AppCompatActivity() {
     //Inicializa el viewBindin
-    private lateinit var binding:ActivityIniciarSesionBinding
+    private lateinit var binding: ActivityIniciarSesionBinding
 
     //Union ViewModel con View
     val viewmodel: IniciarSesionViewModel by viewModels()
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Infla el binding
+        inicializarBinding()
+        realizarPeticion()
+    }
 
+    private fun realizarPeticion() {
+        if (checkForInternet(applicationContext))
+            binding.apply {
+                val correo: String = tietIniciarSesisonCorreo.text.toString()
+                val pass: String = tietIniciarSesionContrasenia.text.toString()
+                btnIniciarSesionIniciarSesion.setOnClickListener {
+                    validarCorreoYContrasenia()
+                    val logIng = LogInRequest(correo, pass)
+
+                    viewmodel.logIn("1h", logIng, this@IniciarSesionView)
+                }
+            }
+    }
+
+    //Validar Correo y contraseña
+    private fun validarCorreo(): Boolean {
+        binding.apply {
+
+            return if (tiet_IniciarSesison_Correo.text.toString().isEmpty()) {
+                tilIniciarSesisonCorreo.error = getString(R.string.campo_vacio)
+                false
+            } else {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(tietIniciarSesisonCorreo.text.toString())
+                        .matches()
+                ) {
+                    tilIniciarSesisonCorreo.isErrorEnabled = false
+                    true
+                } else {
+                    tilIniciarSesisonCorreo.error = getString(R.string.correo_no_valido)
+                    false
+                }
+            }
+        }
+    }
+    private fun validarContrasenia(): Boolean {
+        binding.apply {
+            return if (tietIniciarSesionContrasenia.text.toString().isEmpty()) {
+                tilIniciarSesionContrasenia.error = getString(R.string.campo_vacio)
+                false
+            } else {
+                tilIniciarSesionContrasenia.isErrorEnabled = false
+                true
+            }
+        }
+    }
+    //Valida Correo y contrasenia juntos
+    private fun validarCorreoYContrasenia() {
+        val result = arrayOf(validarCorreo(), validarContrasenia())
+        if (false in result)
+            return
+    }
+
+    //Infla el view Binding
+    private fun inicializarBinding() {
         binding = ActivityIniciarSesionBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-    binding.ivIniciarSesionRegresar.setOnClickListener {
-        lanzarAcivity()
-    }
-
-    }
-
-
-
-
-    fun lanzarAcivity(){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
--- app/src/main/java/mx/kodemia/bankodemiaapp/modules/home/viewmodel/IniciarSesionViewModel.kt
+++ app/src/main/java/mx/kodemia/bankodemiaapp/modules/home/viewmodel/IniciarSesionViewModel.kt
@@ -1,17 +1,65 @@
 package mx.kodemia.bankodemiaapp.modules.home.viewmodel
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import mx.kodemia.bankodemiaapp.data.model.request.LogInRequest
+import mx.kodemia.bankodemiaapp.data.model.response.error.ErrorMessageResponse
+import mx.kodemia.bankodemiaapp.data.model.response.logIn.LoginResponse
+import mx.kodemia.bankodemiaapp.modules.home.view.HomeActivity
 import mx.kodemia.bankodemiaapp.network.service.LogInService
 
-class IniciarSesionViewModel(context: Context): ViewModel() {
-    //Service
-    private val serviceLogin= LogInService(context)
+class IniciarSesionViewModel(context: Context) : ViewModel() {
+    // Servicicio
+    private val serviceLogin = LogInService(context)
 
+    //LoginResponse
+    private val logInResponse = MutableLiveData<LoginResponse>()
 
+    //errores
+    val error = MutableLiveData<ErrorMessageResponse>()
 
+    //Cargando
+    private val cargando = MutableLiveData<Boolean>()
 
+    fun logIn(expires_in: String, logInRequest: LogInRequest, activity: Activity) {
+        //Se lanza la corrutina
+        viewModelScope.launch {
+            //Se lanza una carga
+            cargando.postValue(true)
+            //Se obtiene la respuesta
+            val respuestaLogin = serviceLogin.LogIn(expires_in, logInRequest)
+            //Se lanza una doble validacion
+            try {
+                //Si la respuesta es satisfactoria
+                if (respuestaLogin.isSuccessful) {
+                    //Se actualizara el Mutable LoginResponse
+                    //Voy a cargar la informacion para que le avise a la vista que esta oks
+                    logInResponse.postValue(respuestaLogin.body())
+                    lanzarActivity(activity)
 
+                    //De acuerdo a la documentacion el error 401 es unautorizaed
+                } else if (respuestaLogin.code() == 401) {
+                    Log.e("LoginError", "Unauthorizaed")
+                }
+                cargando.postValue(true)
+            } catch (err : Exception) {
+              //  error.postValue(err.localizedMessage)
+                cargando.postValue(false)
+            }
+        }
+    }
 
-
+    //Si la respuesta es correcta se inicializa activityHome
+    private fun lanzarActivity(activity: Activity) {
+        val intent = Intent(activity, HomeActivity::class.java)
+        activity.startActivity(intent)
+    }
 }
+
+
