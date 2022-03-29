ba190dc96bbf3562d04f3b272f01f7847936ff78
liz <lizthami700@gmail.com>
2022-03-28 21:04:57 -0500

    Cambios de pruebas 28 de marzo

--- app/src/main/java/mx/kodemia/bankodemiaapp/ConfirmacionTransferencia.kt
+++ app/src/main/java/mx/kodemia/bankodemiaapp/ConfirmacionTransferencia.kt
@@ -2,6 +2,7 @@ package com.example.proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.kodemia.bankodemiaapp.R
 
 class ConfirmacionTransferencia : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
