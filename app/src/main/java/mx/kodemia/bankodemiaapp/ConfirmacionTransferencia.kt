c1219cd67a96fe25ba2df069156517b4bb6a1524
liz <lizthami700@gmail.com>
2022-03-28 20:57:31 -0500

    Cambios de funciones

---
+++ app/src/main/java/mx/kodemia/bankodemiaapp/ConfirmacionTransferencia.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmacionTransferencia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmacion_transferencia)
+
+
+    }
+}
---
+++ app/src/main/java/mx/kodemia/bankodemiaapp/EnviarDinero.kt
@@ -0,0 +1,16 @@
+package com.example.proyecto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class EnviarDinero : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enviar_dinero)
+
+
+    }
+}
